1b8d7755c9e6c4d3c490190242712ded55a2865e
wanghanye0 <53504038+wanghanye0@users.noreply.github.com>
2022-05-02 14:04:06 -0400

    update

--- server/server.js
+++ server/server.js
@@ -44,11 +44,40 @@ app.get('/comments', routes.comments);
 
 app.get('/search/animations', routes.search_animations);
 
+<<<<<<< HEAD
 app.get('/all_users', routes.all_user);
 
 app.get('/search/users', routes.search_users);
 
 app.get('/search/single_user', routes.find_single_user)
+=======
+// homework routes
+// Route 1 - register as GET 
+// app.get('/hello', routes.hello)
+//
+// // Route 2 - register as GET
+// app.get('/jersey/:choice', routes.jersey)
+//
+// // Route 3 - register as GET
+// app.get('/matches/:league', routes.all_matches)
+//
+// // Route 4 - register as GET
+// app.get('/players', routes.all_players)
+//
+// // Route 5 - register as GET
+// app.get('/match', routes.match)
+//
+// // Route 6 - register as GET
+// app.get('/player', routes.player)
+//
+// // Route 7 - register as GET
+// app.get('/search/matches', routes.search_matches)
+//
+// // Route 8 - register as GET
+// app.get('/search/players', routes.search_players)
+
+app.get('/users', routes.all_user);
+>>>>>>> add serach result and link to game detail page
 
 app.get("/logout", routes.logout);
 
