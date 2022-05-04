bdb9fd86446e5a81d1bbfc43713ee602f4cde066
wanghanye0 <53504038+wanghanye0@users.noreply.github.com>
2022-05-04 16:00:00 -0400

    connect comments

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -15,7 +15,8 @@ const link = 'http://localhost:8080';
 
 async function getUserInfo(id){
     const info = await axios.get(`${link}/search/single_user?userId=${id}`).catch((err) => { console.log(err); });
-    return info.data.results[0];
+    console.log(info);
+    return info.data.results;
 }
 
 function setupAnimes(nameList, imgList){
@@ -31,26 +32,32 @@ function setupAnimes(nameList, imgList){
     console.log(result);
     return result;
 }
+
+function redirectToAnime(animeId){
+    window.location.replace(`/game?id=${animeId}`);
+}
 export default function Profile() {
     let search = window.location.search;
     let params = new URLSearchParams(search);
     userId = params.get('userId');
     const [info, setInfo] = useState({});
     const [animeInfo, setAnimeInfo] = useState([]);
+    const [comments, setComments] = useState([]);
     if(!window.sessionStorage.getItem('username')){
         window.location.replace("/login");
     }
     useEffect(()=>{
         getUserInfo(userId).then((result) => {
-            setInfo(result);
-            if(result.gender==="Male"){
+            setInfo(result[0]);
+            setComments(result);
+            if(result[0].gender==="Male"){
                 avatar=male;
-            }else if(result.gender==="Female"){
+            }else if(result[0].gender==="Female"){
                 avatar=female;
             }else{
                 avatar=unknown;
             }
-            setAnimeInfo(setupAnimes(result.likeAnime, result.likeAnimeImg));
+            setAnimeInfo(setupAnimes(result[0].likeAnime, result[0].likeAnimeImg));
         });
     }, [])
     console.log(info);
@@ -78,26 +85,16 @@ export default function Profile() {
 
                 <div className="profileInfo">
                     <div className="typeText">Recent Commented</div>
-                    <div className="commentHistoryUnit">
-                        <img src={avatar}/>
-                        <div className="commentHistoryText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci sagittis eu volutpat odio facilisis. Lectus urna duis convallis convallis. Sed sed risus pretium quam vulputate. Viverra maecenas accumsan lacus vel facilisis volutpat. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Orci porta non pulvinar neque laoreet suspendisse interdum. In vitae turpis massa sed elementum. Arcu bibendum at varius vel pharetra vel turpis. Fames ac turpis egestas integer eget aliquet.
-
-                            Nam libero justo laoreet sit amet. At tellus at urna condimentum mattis pellentesque id nibh tortor. Morbi tincidunt ornare massa eget. Vitae elementum curabitur vitae nunc sed velit dignissim. Tristique senectus et netus et malesuada fames ac. Mauris pharetra et ultrices neque ornare aenean. Turpis tincidunt id aliquet risus. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Nisl suscipit adipiscing bibendum est ultricies. Aenean et tortor at risus viverra. Pulvinar etiam non quam lacus suspendisse faucibus. Mauris in aliquam sem fringilla ut morbi. Ac placerat vestibulum lectus mauris ultrices eros in cursus turpis. In vitae turpis massa sed elementum tempus egestas sed sed. Morbi enim nunc faucibus a pellentesque sit. Id aliquet lectus proin nibh. Consequat mauris nunc congue nisi vitae suscipit.
-
-                            Velit egestas dui id ornare. Mollis nunc sed id semper. Ipsum dolor sit amet consectetur adipiscing. At consectetur lorem donec massa sapien faucibus et molestie ac. Condimentum vitae sapien pellentesque habitant morbi. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Tincidunt praesent semper feugiat nibh sed pulvinar. Id velit ut tortor pretium viverra suspendisse potenti nullam ac. Ultrices vitae auctor eu augue ut lectus arcu. Blandit massa enim nec dui nunc. Nunc sed id semper risus in hendrerit gravida. Diam vel quam elementum pulvinar etiam non. Morbi tempus iaculis urna id volutpat lacus laoreet non. In ornare quam viverra orci sagittis eu. Bibendum est ultricies integer quis auctor. Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Libero justo laoreet sit amet cursus sit amet dictum sit.
+                    {(comments != null && comments.map((one) => (
+                        <div className="commentHistoryUnit" onClick={()=>{redirectToAnime(one.animeId)}}>
+                            <div>
+                                <img src={avatar}/>
+                                <div>Rating: {one.rating}</div>
+                            </div>
+                            <div className="commentHistoryText">{one.comments}</div>
                         </div>
-                    </div>
-                    <div className="commentHistoryUnit">
-                        <img src={avatar}/>
-                        <div className="commentHistoryText">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci sagittis eu volutpat odio facilisis. Lectus urna duis convallis convallis. Sed sed risus pretium quam vulputate. Viverra maecenas accumsan lacus vel facilisis volutpat. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Orci porta non pulvinar neque laoreet suspendisse interdum. In vitae turpis massa sed elementum. Arcu bibendum at varius vel pharetra vel turpis. Fames ac turpis egestas integer eget aliquet.
+                    )))}
 
-                            Nam libero justo laoreet sit amet. At tellus at urna condimentum mattis pellentesque id nibh tortor. Morbi tincidunt ornare massa eget. Vitae elementum curabitur vitae nunc sed velit dignissim. Tristique senectus et netus et malesuada fames ac. Mauris pharetra et ultrices neque ornare aenean. Turpis tincidunt id aliquet risus. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Nisl suscipit adipiscing bibendum est ultricies. Aenean et tortor at risus viverra. Pulvinar etiam non quam lacus suspendisse faucibus. Mauris in aliquam sem fringilla ut morbi. Ac placerat vestibulum lectus mauris ultrices eros in cursus turpis. In vitae turpis massa sed elementum tempus egestas sed sed. Morbi enim nunc faucibus a pellentesque sit. Id aliquet lectus proin nibh. Consequat mauris nunc congue nisi vitae suscipit.
-
-                            Velit egestas dui id ornare. Mollis nunc sed id semper. Ipsum dolor sit amet consectetur adipiscing. At consectetur lorem donec massa sapien faucibus et molestie ac. Condimentum vitae sapien pellentesque habitant morbi. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Tincidunt praesent semper feugiat nibh sed pulvinar. Id velit ut tortor pretium viverra suspendisse potenti nullam ac. Ultrices vitae auctor eu augue ut lectus arcu. Blandit massa enim nec dui nunc. Nunc sed id semper risus in hendrerit gravida. Diam vel quam elementum pulvinar etiam non. Morbi tempus iaculis urna id volutpat lacus laoreet non. In ornare quam viverra orci sagittis eu. Bibendum est ultricies integer quis auctor. Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Libero justo laoreet sit amet cursus sit amet dictum sit.
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
