a6dcc85892ff663b845ba47aab689d91eed6417e
yx1215 <yx1215@nyu.edu>
2022-05-10 17:22:39 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# CIS550_Database_Project
+# AnimE
+
+## Group Member
+Bowen Chen (bowchen@seas.upenn.edu, github: bowchen-20)  
+Hanye Wang (hanyew@seas.upenn.edu, github: wanghanye0)  
+Yixiang Xiao (yx1215@seas.upenn.edu, github: yx1215)  
+Shuyan Yu (shuyany@seas.upenn.edu, github: YuRenee)  
+
+
+
+## Launch Instruction
+1. Inside the folder `/server`, run `npm install` to install server dependencies,
+then run `npm start` to start the server.
+2. Inside the folder `/client`, run `npm install` to install client dependencies,
+then run `npm start` to start the client.
+3. In the browser go to `https://localhost:3000` to register/login.
+
+## Demo URL
+`https://youtu.be/JrMTTmuZ1Vg`
+
+
--- server/routes.js
+++ server/routes.js
@@ -662,35 +662,6 @@ async function make_comments(req, res) {
         })
 }
 
-async function make_comments(req, res) {
-    const userId = req.query.userId;
-    const animeId = req.query.animeId;
-    const comment = req.query.comment;
-    const rating = req.query.rating;
-    connection.query(`
-    SELECT * FROM ReviewedBy WHERE userId=? AND animeId=?;`, [userId, animeId]
-        ,
-        function (error, results, fields) {
-            if (error) {
-                console.log(error)
-                res.json({ message: error })
-            } else {
-                if (results.length > 0) {
-                    res.json({ message: `You have commented Anime with Id ${animeId}` })
-                } else {
-                    connection.query(`INSERT INTO ReviewedBy VALUE (?, ?, ?, ?);`, [userId, animeId, comment, rating],
-                        function (error, results, fields) {
-                            if (error) {
-                                res.json({ message: error })
-                            } else {
-                                res.json({ message: "Comment Successful." })
-                            }
-                        })
-                }
-            }
-        })
-}
-
 async function get_avg_score(req, res) {
     const animeId = req.query.animeId;
     let query = `WITH
